15172df89ab14053eea5a00bbd60f909618ebda8
Conor Sheehan <conor@cantinaconsulting.com>
2014-01-06 11:45:39 -0500

    New rails app

---
+++ bread-baker-notes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BreadBakerNotes::Application.config.secret_key_base = '663972b583da48a7fe3d09e8ebda5e402586971f2f43792697ccc7a0de192cf12ce20d3c0d239c9ad8b61c5d06bb9155d988012b1adc25c1d87b6c64480ca972'
---
+++ bread-baker-notes/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BreadBakerNotes::Application.config.session_store :cookie_store, key: '_bread-baker-notes_session'
